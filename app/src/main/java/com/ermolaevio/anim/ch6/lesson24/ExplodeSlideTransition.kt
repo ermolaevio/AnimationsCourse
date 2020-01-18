0fdafe5b2922b2aab90e6f0d015b7b605924cdf4
ermolaevio <ermolaevio@sovcombank.ru>
2020-01-18 21:33:47 +0300

    Lesson 24 added delay effect for second view

--- app/src/main/java/com/ermolaevio/anim/ch6/lesson24/ExplodeSlideTransition.kt
+++ app/src/main/java/com/ermolaevio/anim/ch6/lesson24/ExplodeSlideTransition.kt
@@ -2,6 +2,7 @@ package com.ermolaevio.anim.ch6.lesson24
 
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.transition.Slide
 import android.transition.TransitionManager
 import android.view.Gravity
@@ -14,23 +15,43 @@ import kotlinx.android.synthetic.main.activity_explose_slide_transitions.*
 class ExplodeSlideTransition : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_explose_slide_transitions)
 
         start.setOnClickListener {
             /**
-             * новые транзакции появились в Lolipop
+             * новые транзакции, которые появились в Lolipop
              */
 
             TransitionManager.beginDelayedTransition(
                 findViewById(android.R.id.content),
 //                Explode()
-                Slide(Gravity.BOTTOM) // указывается в какую сторону view будет скрываться
+                Slide(Gravity.START) // указывается в какую сторону view будет скрываться
             )
 
-            image.visibility = if (image.visibility == View.VISIBLE) View.GONE else View.VISIBLE
-            image2.visibility = if (image2.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+            image.visibility = if (image.visibility == View.VISIBLE) {
+                View.INVISIBLE
+            } else {
+                View.VISIBLE
+            }
+
+            /**
+             * откладываем анимацию, чтобы вьюхи в разные стороны разъехались
+             */
+            Handler().postDelayed({
+                TransitionManager.beginDelayedTransition(
+                    findViewById(android.R.id.content),
+//                Explode()
+                    Slide(Gravity.END) // указывается в какую сторону view будет скрываться
+                )
+                image2.visibility = if (image2.visibility == View.VISIBLE) {
+                    View.INVISIBLE
+                } else {
+                    View.VISIBLE
+                }
+            }, 10)
         }
     }
 }
